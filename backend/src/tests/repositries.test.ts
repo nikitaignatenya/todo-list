a44cedf527dae164ec8574b13272d29d900d3614
Nikita Ignatenya <ihnatenya.work@gmail.com>
2026-02-05 20:49:04 +0300

    update

---
+++ backend/src/tests/repositries.test.ts
@@ -0,0 +1,60 @@
+import { connect } from "node:http2";
+import {
+  getDataRep,
+  postDataRep,
+  putDataRep,
+  deleteDataRep,
+} from "../repositories/repository";
+
+const connection = {
+  query: jest.fn(),
+};
+
+jest.mock("pg", () => {
+  return {
+    Pool: jest.fn(() => {
+      return {
+        connect: jest.fn(() => connection),
+      };
+    }),
+  };
+});
+
+describe("testGet", () => {
+  test("1", async () => {
+    connection.query.mockResolvedValue({
+      rows: [{ title: "", description: "", completed: true, createdAt: "" }],
+    });
+    const res = await getDataRep();
+    expect(connection.query).toHaveBeenCalled();
+    expect(res).toEqual([
+      { title: "", description: "", completed: true, createdAt: "" },
+    ]);
+  });
+});
+describe("testPost", () => {
+  test("1", async () => {
+    connection.query.mockResolvedValue({
+      rows: [{ title: "", description: "", completed: true, createdAt: "" }],
+    });
+    const res = await postDataRep("", "", true, "");
+    expect(connection.query).toHaveBeenCalled();
+    expect(res).toEqual([
+      { title: "", description: "", completed: true, createdAt: "" },
+    ]);
+  });
+});
+describe("testDelete", () => {
+  test("1", async () => {
+    connection.query.mockResolvedValue({
+      rows: [
+        { id: 1, title: "", description: "", completed: true, createdAt: "" },
+      ],
+    });
+    const res = await deleteDataRep(1);
+    expect(connection.query).toHaveBeenCalled();
+    expect(res).toEqual([
+      { id: 1, title: "", description: "", completed: true, createdAt: "" },
+    ]);
+  });
+});
